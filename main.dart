5bd1cf8b2061cbe7ba18532c1f6ef74cd4a3cab8
Ruhika <65492460+Ruhika1417@users.noreply.github.com>
2021-02-10 23:14:57 +0530

    I'm Poor

--- main.dart
+++ main.dart
@@ -2,24 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.tealAccent,
-        appBar: AppBar(
-          title: Text('IM RUHIKA'),
-          backgroundColor: Colors.amber,
-        ),
-          body: Center(
-            child: Image(
-              image:AssetImage('images/diamond.png'),
-
-
-              ),
-          ),
-          ),
-        ),
-      );
-
-
-
+    MaterialApp(home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white12,
+         title :Text('IM SO POOR, PLS DONATE BITCOINS!'),
+        centerTitle: true,
+        elevation: 5.33,
+      ),
+      body: Center(
+        child:Image(
+          ///title:Text("money")
+          image:AssetImage('images/bitcoin.webp')
+        )
+      ),
+    ),
+    ),
+  );
 }
