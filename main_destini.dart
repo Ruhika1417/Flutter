9084b26beea3f881952d1fe7ffefd776cdb95c98
Ruhika <65492460+Ruhika1417@users.noreply.github.com>
2021-02-11 23:40:21 +0530

    Destini Challenge

---
+++ main_destini.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'story_brain.dart';
+
+
+
+
+
+void main() => runApp(Destini());
+
+class Destini extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: StoryPage(),
+    );
+  }
+}
+ StoryBrain storyBrain_obj =StoryBrain();
+
+
+
+class StoryPage extends StatefulWidget {
+  _StoryPageState createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Container(
+        decoration: BoxDecoration(
+          image: const DecorationImage(
+            image: AssetImage('images/background.png'),
+            fit: BoxFit.cover,
+          ),
+
+        ),
+
+
+        padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                flex: 12,
+                child: Center(
+                  child: Text(
+                    storyBrain_obj.getStory(),
+                  //10
+
+                    //'Story text will go here.',
+
+                    style: TextStyle(
+                      fontSize: 25.0,
+                    ),
+                  ),
+                  ),
+                ),
+
+
+
+
+
+              Expanded(
+                flex: 2,
+                child: FlatButton(
+                  onPressed: () {
+                    //Choice 1 made by user.
+            setState(() {
+
+                      storyBrain_obj.nextStory(1);
+            });
+                  },
+                  color: Colors.red,
+                  child: Text(
+                    storyBrain_obj.getchoice1( ),
+                                     //13
+
+                   // 'Choice 1',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+                SizedBox(
+                height: 20.0,
+                ),
+
+
+                Expanded(
+                flex: 2,
+                //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
+                //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
+                child: Visibility(
+                  visible: storyBrain_obj.buttonShouldBeVisible(),
+                  child: FlatButton(
+                    onPressed: () {
+                      //Choice 2 made by user.
+
+                   setState(() {
+
+
+
+                        storyBrain_obj.nextStory(2);
+
+
+                   });
+
+                    },
+                    color: Colors.blue,
+                    child: Text(
+                      storyBrain_obj.getchoice2(),  //14
+                      //'Choice 2',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                     ),
+                    ),
+                ),
+                ),
+
+            ],
+          ),
+        ),
+      ),
+
+    );
+  }
+}
+
+//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
+
+//TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
